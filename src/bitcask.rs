d383d05e87472d7891700e4a178129e68d146fea
Nikit <singhnikit16@gmail.com>
2025-07-12 18:53:32 -0400

    use custom errors, results & path refs

--- src/bitcask.rs
+++ src/bitcask.rs
@@ -3,7 +3,7 @@ use std::{
     fs,
     io::{Read, Seek, Write},
     os::unix::fs::FileExt,
-    path::{self, Path},
+    path::{self, Path, PathBuf},
     time::{SystemTime, UNIX_EPOCH},
 };
 
@@ -44,7 +44,7 @@ pub struct Bitcask {
     active_file: fs::File,
     active_file_id: u64,
     writer_pos: u64,
-    data_path: String,
+    data_path: PathBuf,
 }
 
 #[derive(Debug)]
@@ -88,45 +88,51 @@ impl DataFileEntry {
     }
 }
 
-fn gen_file_id(dirpath: &str) -> u64 {
-    fs::create_dir_all(dirpath).expect("Failed to create directory");
-    let dir = path::Path::new(dirpath)
-        .read_dir()
-        .expect("Unable to read directory");
+#[derive(Debug)]
+pub enum BitcaskError {
+    Io(std::io::Error),
+    InvalidFileFormat,
+    KeyNotFound,
+    DirNotFound,
+}
+
+impl From<std::io::Error> for BitcaskError {
+    fn from(error: std::io::Error) -> Self {
+        BitcaskError::Io(error)
+    }
+}
+
+fn gen_file_id<P: AsRef<Path>>(dirpath: P) -> Result<u64, BitcaskError> {
+    let path: &Path = dirpath.as_ref();
+    fs::create_dir_all(path)?;
+    let entries = path.read_dir()?;
     let mut max_id = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards")
         .as_secs();
-    for entry in dir {
-        let entry = entry.expect("Unable to read entry");
-        if let Some(file_id) = get_file_id(&entry.path()) {
-            if file_id > max_id {
-                max_id = file_id;
+    entries.into_iter().for_each(|entry| {
+        if let Ok(entry) = entry {
+            if let Some(file_id) = get_file_id(&entry.path()) {
+                max_id = max_id.max(file_id);
             }
         }
-    }
-    max_id + 1
+    });
+    Ok(max_id + 1)
 }
 
 fn get_file_id(filepath: &Path) -> Option<u64> {
     filepath.file_stem()?.to_str()?.parse::<u64>().ok()
 }
 
-fn build_keydir(dirpath: &str) -> HashMap<Vec<u8>, KeyDir> {
+fn build_keydir<P: AsRef<Path>>(path: P) -> Result<HashMap<Vec<u8>, KeyDir>, BitcaskError> {
+    let dir: &Path = path.as_ref();
     let mut map = HashMap::new();
-    let dir = path::Path::new(dirpath);
     if !dir.exists() {
-        println!("Directory does not exist: {}", dirpath);
-        return map;
+        println!("Directory does not exist: {:?}", dir);
+        return Err(BitcaskError::DirNotFound);
     }
-    let entries = dir
-        .to_path_buf()
-        .read_dir()
-        .expect("Unable to read directory");
-    let mut sorted_entries = entries
-        .into_iter()
-        .collect::<Result<Vec<_>, _>>()
-        .expect("Unable to collect entries");
+    let entries = dir.to_path_buf().read_dir()?;
+    let mut sorted_entries = entries.into_iter().collect::<Result<Vec<_>, _>>()?;
     sorted_entries.sort_by(|a, b| {
         let a_id = get_file_id(&a.path());
         let b_id = get_file_id(&b.path());
@@ -152,8 +158,8 @@ fn build_keydir(dirpath: &str) -> HashMap<Vec<u8>, KeyDir> {
         processed.insert(file_id);
         let hint_filepath = entry.path().with_extension("hint");
         if hint_filepath.exists() {
-            let mut hint_file = fs::File::open(&hint_filepath).expect("Unable to open data file");
-            let file_len = hint_file.metadata().expect("Unable to get metadata").len();
+            let mut hint_file = fs::File::open(&hint_filepath)?;
+            let file_len = hint_file.metadata()?.len();
             let mut buf = [0u8; 8];
 
             while file_pos < file_len {
@@ -193,10 +199,10 @@ fn build_keydir(dirpath: &str) -> HashMap<Vec<u8>, KeyDir> {
                 map.insert(key, map_entry);
             }
         } else {
-            let mut dat_file = fs::File::open(&entry.path()).expect("Unable to open data file");
+            let mut dat_file = fs::File::open(&entry.path())?;
             let mut buf = [0u8; 8];
 
-            let file_len = dat_file.metadata().expect("Unable to get metadata").len();
+            let file_len = dat_file.metadata()?.len();
 
             while file_pos < file_len {
                 // Skip CRC for now!
@@ -216,7 +222,7 @@ fn build_keydir(dirpath: &str) -> HashMap<Vec<u8>, KeyDir> {
                 file_pos += 8;
 
                 let mut key = vec![0u8; key_size as usize];
-                let _ = dat_file.read_exact(&mut key).expect("Unable to read key");
+                let _ = dat_file.read_exact(&mut key)?;
                 file_pos += key_size;
 
                 let map_entry = KeyDir {
@@ -233,49 +239,50 @@ fn build_keydir(dirpath: &str) -> HashMap<Vec<u8>, KeyDir> {
             }
         }
     }
-    map
+    Ok(map)
 }
 
 impl Bitcask {
-    pub fn open(path: &str) -> Self {
-        let file_id = gen_file_id(path);
-        let dirpath = path::Path::new(path);
-        if !dirpath.exists() {
+    pub fn open<P: Into<PathBuf>>(path: P) -> Result<Self, BitcaskError> {
+        let path: PathBuf = path.into();
+        let path: &Path = path.as_ref();
+        let file_id = gen_file_id(path)?;
+        if !path.exists() {
             match fs::create_dir(path) {
-                Ok(_) => println!("Created directory: {}", path),
+                Ok(_) => println!("Created directory: {:?}", path),
                 Err(e) => panic!("Failed to create directory: {}", e),
             }
         }
-        let filepath = dirpath.join(format!("{}.dat", file_id));
+        let filepath = path.join(format!("{}.dat", file_id));
         let active_file = fs::OpenOptions::new()
             .append(true)
             .create(true)
             .open(filepath)
             .expect("Unable to create data file");
-        let key_dir = build_keydir(path);
-        Bitcask {
+        let key_dir = build_keydir(path)?;
+        Ok(Bitcask {
             key_dir,
             active_file,
             active_file_id: file_id,
             writer_pos: 0,
-            data_path: path.to_string(),
-        }
+            data_path: path.to_path_buf(),
+        })
     }
 
-    pub fn get(&self, key: &Vec<u8>) -> Option<Vec<u8>> {
+    pub fn get(&self, key: &Vec<u8>) -> Result<Vec<u8>, BitcaskError> {
         let kd_value = self.key_dir.get(key);
         match kd_value {
             Some(kd) => {
-                let dirpath = path::Path::new(&self.data_path);
+                let dirpath: &Path = self.data_path.as_ref();
                 let filepath = dirpath.join(format!("{}.dat", kd.file_id));
-                let data_file = fs::File::open(filepath).expect("Unable to open data file");
+                let data_file = fs::File::open(filepath)?;
                 let mut buf = vec![0u8; kd.value_size as usize];
                 data_file
                     .read_exact_at(&mut buf, kd.value_pos)
                     .expect("Unable to read data file");
-                return Some(buf);
+                return Ok(buf);
             }
-            None => None,
+            None => Err(BitcaskError::KeyNotFound),
         }
     }
 
@@ -306,8 +313,8 @@ impl Bitcask {
         Some(self.key_dir.keys().collect::<Vec<&Vec<u8>>>())
     }
 
-    pub fn merge(&mut self, dirpath: &str) {
-        let keydir = build_keydir(dirpath);
+    pub fn merge<P: AsRef<Path>>(&mut self, dirpath: P) -> Result<(), BitcaskError> {
+        let keydir = build_keydir(&dirpath)?;
         let mut file_id = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards")
@@ -320,18 +327,16 @@ impl Bitcask {
         let mut merge_file = fs::OpenOptions::new()
             .append(true)
             .create(true)
-            .open(merge_filepath)
-            .expect("Unable to open data file for merging");
+            .open(merge_filepath)?;
         let hint_filepath = path::Path::new(&self.data_path).join(format!("{}.hint", file_id));
         let mut hint_file = fs::OpenOptions::new()
             .append(true)
             .create(true)
-            .open(hint_filepath)
-            .expect("Unable to open hint file for merging");
+            .open(hint_filepath)?;
         let mut write_pos = 0;
         let tombstone = TOMBSTONE.to_vec();
         for (key, _) in &keydir {
-            if let Some(value) = self.get(&key) {
+            if let Ok(value) = self.get(&key) {
                 if value.eq(&tombstone) {
                     continue;
                 }
@@ -353,9 +358,8 @@ impl Bitcask {
                 write_pos += data.len() as u64;
             }
         }
-        let dir = path::Path::new(dirpath)
-            .read_dir()
-            .expect("Unable to read directory");
+        let dirpath: &Path = dirpath.as_ref();
+        let dir = dirpath.read_dir()?;
         for file in dir {
             let filepath = file.expect("Unable to read file").path();
             let id = match get_file_id(&filepath) {
@@ -375,6 +379,7 @@ impl Bitcask {
         self.active_file_id = file_id;
         self.writer_pos = write_pos;
         self.key_dir = keydir;
+        Ok(())
     }
 
     pub fn fold() {
@@ -400,15 +405,15 @@ mod tests {
 
     #[test]
     fn test_get_put() {
-        let mut bitcask = Bitcask::open("/tmp/test1");
+        let mut bitcask = Bitcask::open("/tmp/test1").expect("Failed to open Bitcask");
         bitcask.put(b"key1".to_vec(), b"value1".to_vec());
-        let result = bitcask.get(&b"key1".to_vec());
-        assert_eq!(result, Some(b"value1".to_vec()));
+        let result = bitcask.get(&b"key1".to_vec()).expect("Failed to get value");
+        assert_eq!(result, b"value1".to_vec());
     }
 
     #[test]
     fn test_list_keys() {
-        let bitcask = Bitcask::open("/tmp/test1");
+        let bitcask = Bitcask::open("/tmp/test1").expect("Failed to open Bitcask");
         let keys = bitcask.list_keys();
         assert_eq!(keys, Some(vec![&b"key1".to_vec()]));
     }
@@ -419,30 +424,34 @@ mod tests {
         // bitcask.put(b"key1".to_vec(), b"value1".to_vec());
         // bitcask.put(b"key2".to_vec(), b"value2".to_vec());
 
-        let key_dir = build_keydir("/tmp/test1");
+        let key_dir = build_keydir("/tmp/test1").expect("Failed to build keydir");
         assert_eq!(key_dir.len(), 1);
         assert!(key_dir.contains_key(&b"key1".to_vec()));
     }
 
     #[test]
     fn test_keydir() {
-        let bitcask = Bitcask::open("/tmp/test1");
-        let result = bitcask.get(&b"key1".to_vec());
-        assert_eq!(result, Some(b"value1".to_vec()));
+        let bitcask = Bitcask::open("/tmp/test1").expect("Failed to open Bitcask");
+        let result = bitcask.get(&b"key1".to_vec()).expect("Failed to get value");
+        assert_eq!(result, b"value1".to_vec());
     }
 
     #[test]
     fn test_merge() {
-        let mut bitcask = Bitcask::open("/tmp/test4");
+        let mut bitcask = Bitcask::open("/tmp/test4").expect("Failed to open Bitcask");
         bitcask.put(b"key1".to_vec(), b"value1".to_vec());
         bitcask.put(b"key2".to_vec(), b"value2".to_vec());
 
-        let mut bitcask2 = Bitcask::open("/tmp/test4");
-        bitcask2.merge("/tmp/test4");
+        let mut bitcask2 = Bitcask::open("/tmp/test4").expect("Failed to open Bitcask");
+        let _ = bitcask2.merge("/tmp/test4");
 
-        let bitcask3 = Bitcask::open("/tmp/test4");
-        let val1 = bitcask3.get(&b"key1".to_vec());
-        let val2 = bitcask3.get(&b"key2".to_vec());
+        let bitcask3 = Bitcask::open("/tmp/test4").expect("Failed to open Bitcask");
+        let val1 = bitcask3
+            .get(&b"key1".to_vec())
+            .expect("Failed to get value");
+        let val2 = bitcask3
+            .get(&b"key2".to_vec())
+            .expect("Failed to get value");
 
         let mut files = HashSet::new();
 
@@ -451,7 +460,7 @@ mod tests {
         });
 
         assert_eq!(files.len(), 1);
-        assert_eq!(val1, Some(b"value1".to_vec()));
-        assert_eq!(val2, Some(b"value2".to_vec()));
+        assert_eq!(val1, b"value1".to_vec());
+        assert_eq!(val2, b"value2".to_vec());
     }
 }
